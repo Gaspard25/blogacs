f8ed0409e22ccd7997c97a28bde1c8fee210561c
MagaliH <magali.h@codeur.online>
2018-01-29 11:38:20 +0100

    css page accueil, footer, header

--- footer.php
+++ footer.php
@@ -7,19 +7,15 @@
 			<div class="container">
 				<div class="row foot"> 
 					<div class="col-lg-2 align-self-center">
-						<img src="<?php echo get_template_directory_uri(); ?>/assets/img/logoB.png" class="loup" alt="" width="110" height="200">
+						<img src="<?php echo get_template_directory_uri(); ?>/assets/img/logoB.png" class="loup" alt="LogoCancoicode" width="110" height="200">
 					</div>
 					<div class="col-lg-4 offset-lg-6 align-self-center">
-						<img src="<?php echo get_template_directory_uri(); ?>/assets/img/acs.svg" alt="" width="375" height="100">
+						<img src="<?php echo get_template_directory_uri(); ?>/assets/img/acs.svg" alt="logoACS" width="375" height="100" class="logoACS">
 					</div>
 				</div>
 			</div>
 		</div>
 	</footer>
-
-	
-	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
 	<?php wp_footer(); ?>
 </body>
